c0bd36b69997e85be96bea788e26fc3dbb6446e2
github-action-benchmark <github@users.noreply.github.com>
2024-09-23 09:48:00 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for cf230fb4503c7b6a1a9548d076a8764aef74c638

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,39 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727084848864,
+  "lastUpdate": 1727084880083,
   "repoUrl": "https://github.com/equinor/ert",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "ejah@equinor.com",
-            "name": "Eivind Jahren",
-            "username": "eivindjahren"
-          },
-          "committer": {
-            "email": "ejah@equinor.com",
-            "name": "Eivind Jahren",
-            "username": "eivindjahren"
-          },
-          "distinct": true,
-          "id": "284be7cfdee069971814cabb9d8d775299c3f2a0",
-          "message": "Fix pull request template",
-          "timestamp": "2024-09-16T14:11:37+02:00",
-          "tree_id": "e4079af85c9cd8a6677f4c8ce9b4e7397a7172f4",
-          "url": "https://github.com/equinor/ert/commit/284be7cfdee069971814cabb9d8d775299c3f2a0"
-        },
-        "date": 1726488799005,
-        "tool": "pytest",
-        "benches": [
-          {
-            "name": "tests/performance_tests/test_analysis.py::test_and_benchmark_adaptive_localization_with_fields",
-            "value": 0.1891003928444592,
-            "unit": "iter/sec",
-            "range": "stddev: 0.03555438575818033",
-            "extra": "mean: 5.288196311799998 sec\nrounds: 5"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -928,6 +897,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.052172172669777445",
             "extra": "mean: 5.394156250599986 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ejah@equinor.com",
+            "name": "Eivind Jahren",
+            "username": "eivindjahren"
+          },
+          "committer": {
+            "email": "ejah@equinor.com",
+            "name": "Eivind Jahren",
+            "username": "eivindjahren"
+          },
+          "distinct": true,
+          "id": "cf230fb4503c7b6a1a9548d076a8764aef74c638",
+          "message": "Upload test results to codecov",
+          "timestamp": "2024-09-23T11:46:12+02:00",
+          "tree_id": "31cd1fad6d2c9d8e46824bac8bd5b4f12ae182a6",
+          "url": "https://github.com/equinor/ert/commit/cf230fb4503c7b6a1a9548d076a8764aef74c638"
+        },
+        "date": 1727084879441,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/performance_tests/test_analysis.py::test_and_benchmark_adaptive_localization_with_fields",
+            "value": 0.18794283172235418,
+            "unit": "iter/sec",
+            "range": "stddev: 0.045694601942696866",
+            "extra": "mean: 5.320766909999998 sec\nrounds: 5"
           }
         ]
       }
