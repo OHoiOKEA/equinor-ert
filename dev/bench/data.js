226eb85df65cb83ace9bf665b1bd8511d56493b9
github-action-benchmark <github@users.noreply.github.com>
2024-10-14 08:18:20 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 12d2b53e70fdd0341e208e120e3786ca07c2b998

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,39 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728892329759,
+  "lastUpdate": 1728893900756,
   "repoUrl": "https://github.com/equinor/ert",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "levje@equinor.com",
-            "name": "larsevj",
-            "username": "larsevj"
-          },
-          "committer": {
-            "email": "60844986+larsevj@users.noreply.github.com",
-            "name": "Lars Evje",
-            "username": "larsevj"
-          },
-          "distinct": true,
-          "id": "371eb58e8ccab70046346003c2e13791f61aa6e0",
-          "message": "Update pre-commit versions",
-          "timestamp": "2024-10-08T13:49:41+02:00",
-          "tree_id": "924f80f34d3b7fc4f1446652f3876bf5daf9378b",
-          "url": "https://github.com/equinor/ert/commit/371eb58e8ccab70046346003c2e13791f61aa6e0"
-        },
-        "date": 1728388286820,
-        "tool": "pytest",
-        "benches": [
-          {
-            "name": "tests/ert/performance_tests/test_analysis.py::test_and_benchmark_adaptive_localization_with_fields",
-            "value": 0.19079747338218023,
-            "unit": "iter/sec",
-            "range": "stddev: 0.0222789888644443",
-            "extra": "mean: 5.241159551399994 sec\nrounds: 5"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -930,6 +899,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.013038809775689945",
             "extra": "mean: 5.236881575200004 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "levje@equinor.com",
+            "name": "larsevj",
+            "username": "larsevj"
+          },
+          "committer": {
+            "email": "yngve-sk@users.noreply.github.com",
+            "name": "Yngve S. Kristiansen",
+            "username": "yngve-sk"
+          },
+          "distinct": true,
+          "id": "12d2b53e70fdd0341e208e120e3786ca07c2b998",
+          "message": "Give in full file path for polars write_parquet",
+          "timestamp": "2024-10-14T10:16:26+02:00",
+          "tree_id": "102b766cc878795d11ca0139f3396a47f55bd752",
+          "url": "https://github.com/equinor/ert/commit/12d2b53e70fdd0341e208e120e3786ca07c2b998"
+        },
+        "date": 1728893899897,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/ert/performance_tests/test_analysis.py::test_and_benchmark_adaptive_localization_with_fields",
+            "value": 0.1929227660734505,
+            "unit": "iter/sec",
+            "range": "stddev: 0.027854687021076085",
+            "extra": "mean: 5.183421429999998 sec\nrounds: 5"
           }
         ]
       }
