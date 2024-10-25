3202b83d8b04f398c00acefefcf00d93306b5bac
github-action-benchmark <github@users.noreply.github.com>
2024-10-25 10:34:47 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for cdee9290d42d9fb11ccc6fc4d0efdd6e6d71069f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,39 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729849779659,
+  "lastUpdate": 1729852487397,
   "repoUrl": "https://github.com/equinor/ert",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "feda.curic@gmail.com",
-            "name": "Feda Curic",
-            "username": "dafeda"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "96b8fe11da8fdeaf83cd3608f80d86b993d67212",
-          "message": "Update type of usecols in _read_excel\n\nusecols takes str, list-like or callable\r\nUpdate outdated docstring.",
-          "timestamp": "2024-10-17T11:39:33+02:00",
-          "tree_id": "c8ce1ed81bb6d3241ae88a8adf0ef11890eba6ee",
-          "url": "https://github.com/equinor/ert/commit/96b8fe11da8fdeaf83cd3608f80d86b993d67212"
-        },
-        "date": 1729158081974,
-        "tool": "pytest",
-        "benches": [
-          {
-            "name": "tests/ert/performance_tests/test_analysis.py::test_and_benchmark_adaptive_localization_with_fields",
-            "value": 0.1944061915561054,
-            "unit": "iter/sec",
-            "range": "stddev: 0.045912425825283924",
-            "extra": "mean: 5.1438690917999965 sec\nrounds: 5"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -930,6 +899,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.02303164856804989",
             "extra": "mean: 5.158150960999995 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yngve-sk@users.noreply.github.com",
+            "name": "Yngve S. Kristiansen",
+            "username": "yngve-sk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cdee9290d42d9fb11ccc6fc4d0efdd6e6d71069f",
+          "message": "Handle empty responses in MeasuredData\n\nreturn empty df for measureddata w/o responses",
+          "timestamp": "2024-10-25T10:32:56Z",
+          "tree_id": "c7e14aeb0418c9056c888620b1c51c8ccff6e6f3",
+          "url": "https://github.com/equinor/ert/commit/cdee9290d42d9fb11ccc6fc4d0efdd6e6d71069f"
+        },
+        "date": 1729852486911,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/ert/performance_tests/test_analysis.py::test_and_benchmark_adaptive_localization_with_fields",
+            "value": 0.19067178404012128,
+            "unit": "iter/sec",
+            "range": "stddev: 0.02336097942772396",
+            "extra": "mean: 5.244614482600002 sec\nrounds: 5"
           }
         ]
       }
